3c2780f2c793d9ee25c6bb1ca01953a137b2401a
gabrieloarcas <you@example.com>
2019-09-14 21:38:23 +0100

    nav by categories and showing votes on the main page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,12 @@ class PostsController < ApplicationController
   def index
     # @posts = Post.all.order(created_at: :desc)
 
-  if params[:category].blank?
-    @posts = Post.all.order("created_at DESC")
-    else
-    @category_id = Category.find_by(name: params[:category]).id
-    @posts = Post.where(category_id: @category_id).order("created_at DESC")
+    if params[:category].blank?
+      @posts = Post.all.order("created_at DESC")
+      else
+      @category_id = Category.find_by(name: params[:category]).id
+      @posts = Post.where(category_id: @category_id).order("created_at DESC")
     end
-
 
   end
 
